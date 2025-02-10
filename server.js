933a52ff54de7a2dc8aeea2927870962ece60256
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-02-10 12:09:52 +0100

    aggiunta getione errori server e path sbagliato

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express")
 const app = express()
 const port = 3000
 const postsRouter = require("./routers/posts")
+const errorHandler = require("./middleware/errorHandler")
+const notFound = require("./middleware/notFound")
 
 
 app.use(express.static("public"));
@@ -11,6 +13,10 @@ app.get("/", (req, res) => {
 })
 
 app.use("/posts", postsRouter)
+
+app.use(errorHandler)
+
+app.use(notFound)
 
 
 app.listen(port, ()=> {
