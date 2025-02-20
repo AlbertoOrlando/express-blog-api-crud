11f0fd9a921e8d9499d39ec036d562cd6535616d
AlbertoOrlando <albertoorlandowork@gmail.com>
2025-02-20 18:31:39 +0100

    install cors add on server.js and declare authorized route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const port = 3000
 const postsRouter = require("./routers/posts")
 const errorHandler = require("./middleware/errorHandler")
@@ -7,6 +8,10 @@ const notFound = require("./middleware/notFound")
 
 
 app.use(express.static("public"));
+
+app.use(corsa({
+    origin: "http://localhost:5174/"
+}))
 
 app.get("/", (req, res) => {
     res.send("Home del mio Blog")
@@ -19,6 +24,6 @@ app.use(errorHandler)
 app.use(notFound)
 
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
